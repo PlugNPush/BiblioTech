db9b1aaeafa4b3c0723a52a764ee3708d3f1ba79
RomainMarques <77971683+RomainMarques@users.noreply.github.com>
2023-04-26 13:54:14 +0200

    début front

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import MainPage from "./MainPage";
+
+
+function App() {
+  return (
+    <div>
+      <MainPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Component } from "react";
+
+class MainPage extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return <div>MainPage</div>
+    }
+}
+
+export default MainPage
