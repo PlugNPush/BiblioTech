a7ebebd737adb25e908d4f15f6799a627e836019
RomainMarques <77971683+RomainMarques@users.noreply.github.com>
2023-06-03 16:51:13 +0200

    remove proxy

--- my-app/src/setupProxy.js
+++
@@ -1,7 +0,0 @@
-const {createProxyMiddleware} = require("http-proxy-middleware");
-module.exports = app => {
-    app.use(createProxyMiddleware("/books/v1/", {
-        target:'https://www.googleapis.com',
-        changeOrigin: true
-    }))
-}
